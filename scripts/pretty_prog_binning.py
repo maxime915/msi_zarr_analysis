215b504cdca8a0bdac178d92904c8dd6579d7c8a
Maxime <maxime.amodei@gmail.com>
2022-05-24 21:24:29 +0200

    add lipid marks in the progressive binning diagram

--- scripts/pretty_prog_binning.py
+++ scripts/pretty_prog_binning.py
@@ -8,7 +8,7 @@ import pandas as pd
 assert len(sys.argv) == 2, "expected exactly one argument"
 path = sys.argv[1]
 assert path[-4:] == ".npz", "expected a numpy z archive"
-prefix = "fine_saved_prog_binning_"
+prefix = "saved_prog_binning_"
 assert path[: len(prefix)] == prefix, f"filename should start with {prefix=}"
 
 suffix = path[len(prefix) : -4]
@@ -29,6 +29,21 @@ collected_scores = archive["collected_scores"]
 
 df = pd.read_csv("mz value + lipid name.csv", sep=None, engine="python")
 
+lipids_of_interest = df[
+    df["Name"].isin(
+        [
+            "DPPC",
+            "PLPC",
+            "LysoPPC",
+            "PAzPC",
+            "PDHPC",
+            "PAPC",
+            "SLPC",
+            "LysoSPC/  PAF -O-16:0",
+        ]
+    )
+]
+
 n_iteration = len(collected_scores)
 blueish = cycle([0.5, 0.7])
 
@@ -36,12 +51,18 @@ blueish = cycle([0.5, 0.7])
 fig, ax = plt.subplots(figsize=(10, 5))
 fig.suptitle(f"Progressive Binning ({cls_description})")
 
-ax.get_yaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
 ax.invert_yaxis()
+
+cmap = plt.cm.get_cmap("Accent", len(lipids_of_interest)+1)
 
 # show known lipids
 for mz in df["m/z"]:
-    ax.axvline(mz, color=(0.9, 0.1, 0.1, 0.3))
+    ax.axvline(mz, alpha=0.2, c=cmap(len(lipids_of_interest)))
+
+# show more interesting lipids
+for i, tpl in enumerate(lipids_of_interest.itertuples()):
+    ax.axvline(tpl[1], label=tpl[3]+f" ({tpl[1]})", alpha=0.5, c=cmap(i))
 
 for i in range(collected_bin_hi.shape[0]):
     bin_lo = collected_bin_lo[i]
@@ -52,8 +73,9 @@ for i in range(collected_bin_hi.shape[0]):
 
 ax.set_xticks(range(100, 1200, 50))
 
+plt.legend()
 plt.tight_layout()
-fig.savefig(f"fine_progressive_binning{suffix}.svg")
+fig.savefig(f"progressive_binning{suffix}.svg")
 # plt.show()
 
 # learning curve
@@ -67,5 +89,5 @@ ax.legend()
 ax.set_ylim((0, 100))
 
 plt.tight_layout()
-fig.savefig(f"fine_accuracy_evolution_{suffix}.png")
+fig.savefig(f"accuracy_evolution_{suffix}.png")
 # plt.show()
