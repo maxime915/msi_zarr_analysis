0fd1d811fd1da981679af2ccbec89609169806fd
Maxime <maxime.amodei@gmail.com>
2022-05-24 21:36:36 +0200

    improve display of error

--- scripts/error_registration.py
+++ scripts/error_registration.py
@@ -2,14 +2,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-ds = pd.read_csv("res_floor.csv", index_col=None)
+import pathlib
+import sys
+
+source = "res_floor.csv" if len(sys.argv) < 2 else sys.argv[1]
+
+ds = pd.read_csv(source, index_col=None)
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
 # problem: tol was 0.001 but mean error is close to 0.015
 
-ax1.boxplot(ds.true_scale - ds.pred_scale)
-ax1.set_title("Scale Error\n(true - prediction)")
+ax1.boxplot((ds.true_scale - ds.pred_scale) / ds.true_scale)
+ax1.set_title("Scale Relative Error\n(true - prediction) / true")
 ax1.get_xaxis().set_visible(False)
 
 ax2.boxplot(ds.true_y - ds.pred_y)
@@ -21,4 +26,4 @@ ax3.set_title("Coordinate (x) Error\n(true - prediction)")
 ax3.get_xaxis().set_visible(False)
 
 fig.tight_layout()
-fig.savefig("res_floor.png")
+fig.savefig(pathlib.Path(source).with_suffix(".png"))
