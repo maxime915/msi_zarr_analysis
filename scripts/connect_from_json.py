0cab675e5bf14b2505040b21dc8f4f010cb517f2
Maxime <maxime.amodei@gmail.com>
2022-09-28 10:21:31 +0200

    feat: connect from key-value parameters
    To avoid using a JSON file if connecting to a
    temporary server

--- scripts/connect_from_json.py
+++ scripts/connect_from_json.py
@@ -1,16 +1,22 @@
-
-from cytomine import Cytomine
 import json
 
-def connect(
-    json_path: str = "config_cytomine.json"
-) -> Cytomine:
+from cytomine import Cytomine
+
+
+def connect(json_path: str = "config_cytomine.json") -> Cytomine:
 
     with open(json_path) as config_file:
         config_data = json.loads(config_file.read())
         host_url = config_data["HOST_URL"]
         pub_key = config_data["PUB_KEY"]
         priv_key = config_data["PRIV_KEY"]
-    
-    return Cytomine.connect(host_url, pub_key, priv_key)
 
+    return connect_with_params(host_url, pub_key, priv_key)
+
+
+def connect_with_params(
+    host_url: str,
+    pub_key: str,
+    priv_key: str,
+) -> Cytomine:
+    return Cytomine.connect(host_url, pub_key, priv_key)
